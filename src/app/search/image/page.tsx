8dcbe43c1663666e2f70342c0aef49dc7e0df3aa
NTh-Hai <thehai318@gmail.com>
2023-03-16 20:00:27 +0700

    fix: fix prerender error on image search page

--- src/app/search/image/page.tsx
+++ src/app/search/image/page.tsx
@@ -38,7 +38,7 @@ export default async function Page({
     startIndex = startIndex[-1];
   }
 
-  let query = searchParams?.query ?? "";
+  let query = searchParams?.query ?? " ";
   if (typeof query === "object") {
     query = query.join(" ");
   }
